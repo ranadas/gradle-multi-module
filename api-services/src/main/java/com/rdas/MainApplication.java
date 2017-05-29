a0257eacbd07716febfb7523c0b7ffc290c4233d
rdas <rana.das@hrblock.com>
2017-05-29 19:53:52 +0100

    adding swagger2

--- api-services/src/main/java/com/rdas/MainApplication.java
+++ api-services/src/main/java/com/rdas/MainApplication.java
@@ -1,8 +1,10 @@
-package com.rdas.grmultimodule;
+package com.rdas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class MainApplication {
 
---
+++ api-services/src/main/java/com/rdas/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.rdas.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by x148128 on 29/05/2017.
+ *
+ */
+@Api(value = "2. RestController", description = " A SIMPLE hello Controller")
+@RestController
+public class HelloController {
+
+    /**
+     * http://localhost:8081/swagger-ui.html
+     * localhost:8081/hello
+     * @return
+     */
+    @ApiOperation(value = "Hello.", notes = "Says Hello.")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public    @ResponseBody
+    String greeting() {
+        return "Hello World.";
+    }
+}
