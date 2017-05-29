b235b80cace1691029ea1025ae9eeb392b9d4351
rdas <rana.das@hrblock.com>
2017-05-29 19:36:44 +0100

    Using libraries gradle from root

--- api-services/src/test/java/com/rdas/grmultimodule/MainApplicationTests.java
+++ api-services/src/test/java/com/rdas/grmultimodule/MainApplicationTests.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.rdas.entity.Product;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        Product product = Product.builder().name("test product").build();
+    }
 
 }
