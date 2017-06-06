c8b7544febe9e3bf60cd15148bae1a3c5389054d
Rana Das <rana.pratap.das@gmail.com>
2017-06-06 06:41:47 +0100

    Adding create Product script

--- derby-server/src/main/resources/create_tables.sql
+++ derby-server/src/main/resources/create_tables.sql
@@ -5,3 +5,11 @@ CREATE TABLE security_question (
   created_date timestamp DEFAULT current_timestamp,
   last_modified timestamp DEFAULT current_timestamp
 );
+
+CREATE TABLE Product (
+  id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+  name VARCHAR(200) NOT NULL,
+  price NUMERIC not null,
+  created_date timestamp DEFAULT current_timestamp,
+  last_modified timestamp DEFAULT current_timestamp
+);
