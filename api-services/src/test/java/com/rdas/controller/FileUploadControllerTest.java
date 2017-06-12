2cb8cefe186ed8933c2a9292231ab1f1b8b8b719
Rana Das <rana.pratap.das@gmail.com>
2017-06-12 06:40:18 +0100

    FileUplosd Test - wip

--- api-services/src/test/java/com/rdas/controller/FileUploadControllerTest.java
+++ api-services/src/test/java/com/rdas/controller/FileUploadControllerTest.java
@@ -1,9 +1,12 @@
 package com.rdas.controller;
 
+import com.jayway.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +22,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class FileUploadControllerTest {
 
     private final Logger logger = LoggerFactory.getLogger(FileUploadControllerTest.class);
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void init() {
+        RestAssured.port = port;
+    }
+
     @Test
     public void uploadASingleFile() {
 
