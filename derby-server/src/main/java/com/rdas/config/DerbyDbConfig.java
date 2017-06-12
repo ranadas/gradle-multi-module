81d171f62529cc0edcd4bc0cbcac7eb800f7b774
rdas <rana.das@hrblock.com>
2017-06-12 11:14:29 +0100

    added chalk for logging

--- derby-server/src/main/java/com/rdas/config/DerbyDbConfig.java
+++ derby-server/src/main/java/com/rdas/config/DerbyDbConfig.java
@@ -1,5 +1,6 @@
 package com.rdas.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,25 +16,45 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class DerbyDbConfig {
-	@Primary
-	@Bean(name = "dataSource", destroyMethod = "")
-	public DataSource dataSource() {
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder
-				.setType(EmbeddedDatabaseType.DERBY)
-				.addScript("create_tables.sql")
-				.build();
-		return db;
-	}
+    @Primary
+    @Bean(name = "dataSource", destroyMethod = "")
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.DERBY)
+                .addScript("create_tables.sql")
+                .build();
+        return db;
+    }
 
 
-	@Bean(name = "derbyDataSource", destroyMethod = "")
-	public DataSource derbyDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
-		dataSource.setUrl("jdbc:derby:temp\\database\\test01;create=true");
-		dataSource.setUsername("");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+    //@Bean(name = "derbyServerDataSource", destroyMethod = "")
+    public DataSource derbyServerDataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
+//		dataSource.setUrl("jdbc:derby:temp\\database\\test01;create=true");
+//		dataSource.setUsername("");
+//		dataSource.setPassword("");
+//		return dataSource;
+        return DataSourceBuilder
+                .create()
+//                .url("jdbc:h2:D:/work/workspace/fork/gs-serving-web-content/initial/data/fdata;DATABASE_TO_UPPER=false")
+                //.username("derbyuser")
+                //.password("derbypass")
+                .driverClassName("org.apache.derby.jdbc.EmbeddedDriver")
+                .build();
+    }
+
+    //@Bean(name = "derbyClientDataSource", destroyMethod = "")
+    public DataSource derbyClientDataSource() {
+/*
+spring.datasource.url=jdbc:derby://10.12.1.1:1527/example1;create=true
+spring.datasource.username=test1
+spring.datasource.password=pass1
+spring.datasource.driver-class-name=org.apache.derby.jdbc.ClientDriver
+ */
+        return DataSourceBuilder
+                .create()
+                .build();
+    }
 }
