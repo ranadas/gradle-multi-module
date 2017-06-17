bf709e0dc7fcb3c9a9077dbbd53004fca7177765
Rana Das <rana.pratap.das@gmail.com>
2017-06-17 18:27:04 +0100

    Adding dev tools to api services

--- api-services/src/main/java/com/rdas/controller/HelloController.java
+++ api-services/src/main/java/com/rdas/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.rdas.controller;
 
+import com.github.tomaslanger.chalk.Chalk;
 import com.rdas.util.Loggable;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "2. RestController", description = " A SIMPLE hello Controller")
 @RestController
 public class HelloController {
-
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
     /**
      * localhost:8088/apislv/hello
      */
@@ -24,6 +27,7 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public    @ResponseBody
     String greeting() {
+        logger.info("{}" , Chalk.on("\tInvoking greeting ").green());
         return "Hello World.";
     }
 }
