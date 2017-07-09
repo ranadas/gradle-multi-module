73a810fe0e6c84c486555913892647a25def0489
Rana Das <rana.pratap.das@gmail.com>
2017-07-09 06:19:28 +0100

    Add main runner class add yaml for the language code values

---
+++ iconic/src/main/java/com/iptranslator/ApplicationMain.java
@@ -0,0 +1,27 @@
+package com.iptranslator;
+
+import com.iptranslator.utils.LanguageCodeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rdas on 08/07/2017.
+ */
+@SpringBootApplication
+public class ApplicationMain {
+
+    @Autowired
+    private LanguageCodeResolver languageCodeResolver;
+
+    public static void main(String[] args) {
+//        SpringApplication.run(ApplicationMain.class, args);
+        SpringApplication application = new SpringApplication(ApplicationMain.class);
+//        application.setWebEnvironment(false);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
+}
